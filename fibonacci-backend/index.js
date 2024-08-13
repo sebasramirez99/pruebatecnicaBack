8ff1501f011da7220a1b6f006bd12187ebad947c
sebasramirez99 <sebastianbuenoramirez@gmail.com>
2024-08-13 12:53:09 -0500

    verce.json

--- fibonacci-backend/index.js
+++ fibonacci-backend/index.js
@@ -100,7 +100,5 @@ app.post('/send-email', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// Exporta la aplicación para que Vercel pueda manejarla como una función serverless
+module.exports = app;
